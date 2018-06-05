dee45412c923260f3108ee96c30dc381bf2b9a25
rei <rei@AMS-GYM-EDV1-01.gym.ams.local>
2018-06-05 15:44:52 +0200

    Access-DB ucanaccess jars DBTest-File

---
+++ Gui2/src/db/DBTest.java
@@ -0,0 +1,32 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBTest {
+
+	public static void main(String[] args) {
+		try {
+			new DBTest();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public DBTest() throws SQLException {
+		// Verbindung zur DB...
+		// Pfad zur Access-DB-Datei kann zu Hause anders sein!
+		Connection conn = 
+				DriverManager.
+				getConnection
+				("jdbc:ucanaccess://c:/users/rei/dbtest.accdb;memory=false");
+		// Insert ausführen
+		Statement stmt = conn.createStatement();
+		stmt.execute("INSERT INTO address ( vorname, nachname, ort, plz ) "
+				+ "VALUES ('hansi2', 'maier', 'wien', 1120)");
+		//
+	}
+
+}
